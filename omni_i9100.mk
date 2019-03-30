6ed764d04cf5ec6212bbc67414625a5426cef80c
Dan Santos <dansantosbe@gmail.com>
2019-03-30 01:12:50 -0300

    i9100: TWRP: Initial configs for android-9.0 branch

--- omni_i9100.mk
+++ omni_i9100.mk
@@ -23,12 +23,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-# Time Zone data for Recovery
-PRODUCT_COPY_FILES += \
-    bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
+# Overlay
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=220
 
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := i9100
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl \
+    sensors.exynos4
+
+# Device identifier. This must come after all inclusions
 PRODUCT_NAME := omni_i9100
-PRODUCT_BRAND := samsung
+PRODUCT_DEVICE := i9100
+PRODUCT_BRAND := Samsung
+PRODUCT_MODEL := GT-I9100
 PRODUCT_MANUFACTURER := samsung
+
+# Set build fingerprint / ID / Prduct Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-I9100 TARGET_DEVICE=GT-I9100
+BUILD_FINGERPRINT=samsung/GT-I9100/GT-I9100:4.1.2/JZO54K/I9100XWMS2:user/release-keys
+PRIVATE_BUILD_DESC="GT-I9100-user 4.1.2 JZO54K I9100XWMS2 release-keys"
